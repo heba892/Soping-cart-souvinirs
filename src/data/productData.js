ed01aaadaf0bc7bfeb1d5ea98068095a3c07d301
heba892 <hebagamal231199@gmail.com>
2023-06-07 04:53:42 +0300

    title added

--- src/data/productData.js
+++ src/data/productData.js
@@ -1,54 +1,54 @@
 import img1 from '../assets/97 (1).webp'
 import img2 from '../assets/2.jpg'
-import img3 from '../assets/3.jpg'
+import img3 from '../assets/94.webp'
 import img4 from '../assets/69 (1).webp'
 const productData = [
     {
         id:1,
         Image:img1,
-        title:'Books',
+        title:'Portrait',
         price:'150'
 
     },
     {
         id:2,
         Image:img2,
-        title:'Books',
+        title:'Accessories',
         price:'150'
 
     },
     {
         id:3,
         Image:img3,
-        title:'Books',
+        title:'Souviners',
         price:'150'
 
     },
     {
         id:4,
         Image:img4,
-        title:'Books',
+        title:'Papyrus',
         price:'150'
 
     },
     {
         id:5,
         Image:img1,
-        title:'Books',
+        title:'Portrait',
         price:'150'
 
     },
     {
         id:6,
         Image:img2,
-        title:'Books',
+        title:'Accessories',
         price:'150'
 
     },
     {
         id:7,
         Image:img3,
-        title:'Books',
+        title:'Souviners',
         price:'150'
 
     },
@@ -56,7 +56,7 @@ const productData = [
     {
         id:8,
         Image:img4,
-        title:'Books',
+        title:'Papyrus',
         price:'150'
 
     },
